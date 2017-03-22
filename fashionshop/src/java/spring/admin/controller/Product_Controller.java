23a003166055a5861e5784a8428e8a3baa5b5276
anquangvinh <anquangvinh1992@gmail.com>
2017-03-22 20:22:48 +0700

    - Thêm sẵn package ejb, interceptor - Thay đổi cấu trúc thư mục images

--- fashionshop/src/java/spring/admin/controller/Product_Controller.java
+++ fashionshop/src/java/spring/admin/controller/Product_Controller.java
@@ -5,55 +5,91 @@
  */
 package spring.admin.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import spring.ejb.ProductStateLessBeanLocal;
+import spring.entity.Categories;
 
 @Controller
-@RequestMapping(value = "/admin/product/")
+@RequestMapping(value = "/admin/")
 public class Product_Controller {
 
-    @RequestMapping(value = "category")
-    public String productCateList() {
+    ProductStateLessBeanLocal productStateLessBean = lookupProductStateLessBeanLocal();
+    
+    /* 
+     *    CATEGORY TREATMENT 
+     */
+    @RequestMapping(value = "product-category")
+    public String productCateList(ModelMap model) {
+        model.addAttribute("cateList", productStateLessBean.categoryList());
         return "admin/pages/product-category-list";
     }
-    
-    @RequestMapping(value = "category/create")
+
+    @RequestMapping(value = "product-category/create")
     public String productCateAdd() {
         return "admin/pages/product-category-add";
     }
-    
-    @RequestMapping(value = "category/edit")
-    public String productCateUpdate() {
+
+    @RequestMapping(value = "product-category/{cateNameNA}-{cateID}", method = RequestMethod.GET)
+    public String productCateUpdate(ModelMap model, @PathVariable("cateID") Integer cateID) {
+        Categories targetCate = productStateLessBean.findCategoryByID(cateID);
+        model.addAttribute("targetCate", targetCate);
         return "admin/pages/product-category-update";
     }
-    
-    @RequestMapping(value = "subcategory")
-    public String productSubCateList() {
+
+    /* 
+     *    SUB-CATEGORY TREATMENT 
+     */
+    @RequestMapping(value = "product-subcategory")
+    public String productSubCateList(ModelMap model) {
+        model.addAttribute("subCateList", productStateLessBean.subCategoryList());
         return "admin/pages/product-subcategory-list";
     }
-    
-    @RequestMapping(value = "subcategory/create")
+
+    @RequestMapping(value = "product-subcategory/create")
     public String productSubCateAdd() {
         return "admin/pages/product-subcategory-add";
     }
-    
-    @RequestMapping(value = "subcategory/edit")
+
+    @RequestMapping(value = "product-subcategory/edit")
     public String productSubCateUpdate() {
         return "admin/pages/product-subcategory-update";
     }
     
-    @RequestMapping(value = "list")
-    public String productList() {
+    /* 
+     *    PRODUCT TREATMENT 
+     */
+    @RequestMapping(value = "product")
+    public String productList(ModelMap model) {
+        model.addAttribute("productList", productStateLessBean.productList());
         return "admin/pages/product-list";
     }
-    
-    @RequestMapping(value = "create")
+
+    @RequestMapping(value = "product/create")
     public String productAdd() {
         return "admin/pages/product-add";
     }
-    
-    @RequestMapping(value = "edit")
+
+    @RequestMapping(value = "product/edit")
     public String productUpdate() {
         return "admin/pages/product-update";
+    }
+
+    private ProductStateLessBeanLocal lookupProductStateLessBeanLocal() {
+        try {
+            Context c = new InitialContext();
+            return (ProductStateLessBeanLocal) c.lookup("java:global/fashionshop/ProductStateLessBean!spring.ejb.ProductStateLessBeanLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
     }
 }
