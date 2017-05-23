3fbc91c7b8ca2fa79dc71944acd9b1400b4a9d9e
anquangvinh <anquangvinh1992@gmail.com>
2017-05-23 09:06:55 +0700

    product commit 230520179h06

--- fashionshop/src/java/spring/entity/SubCategories.java
+++ fashionshop/src/java/spring/entity/SubCategories.java
@@ -8,7 +8,9 @@ package spring.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -62,7 +64,17 @@ public class SubCategories implements Serializable {
     public List<Products> getProductList() {
         return productList;
     }
-
+    
+    public List<Products> getProductListWorking(){
+        List<Products> productListWorking = new ArrayList<>();
+        for (Products p : productList) {
+            if(p.getStatus() == 1){
+                productListWorking.add(p);
+            }
+        }
+        return productListWorking;
+    }
+    
     public void setProductList(List<Products> productList) {
         this.productList = productList;
     }
