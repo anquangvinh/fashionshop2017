95dfab03e7c8c34c3558bbe3dde77b7a9ff8475e
anquangvinh <anquangvinh1992@gmail.com>
2017-05-23 16:34:32 +0700

    reupload project 23052017 16h34

---
+++ fashionshop/src/java/spring/ejb/BlogsSBLocal.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spring.ejb;
+
+import java.util.List;
+import javax.ejb.Local;
+import spring.entity.Blogs;
+
+/**
+ *
+ * @author Phan
+ */
+@Local
+public interface BlogsSBLocal {
+
+    public List<Blogs> getAllBlogs();
+
+    public List<Blogs> getAllBlogsIndex();
+
+    public List<Blogs> getAllBlogsIndexMonth();
+
+    public List<Blogs> getAllBlogsAdmin();
+
+    List<Blogs> getListBlogsByCategory(int blogCateID);
+
+    public List<Blogs> getAllBlogsByMonth(int month);
+
+    boolean blogAdd(Blogs newBlogs);
+
+    Blogs findBlogsByID(int id);
+
+    boolean editBlogs(Blogs targetBlogs);
+
+    List<Blogs> findBlogsByTitle(String blogTitle, List<Integer> monthList);
+
+//    BlogCategories findBlogCategoryByBlogCateName(String blogCateName);
+    int deleteBlog(Blogs blog);
+    
+      public Integer getAllNumberBlogsInCate(int blogCateID);
+    
+
+}
